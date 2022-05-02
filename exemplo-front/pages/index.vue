79ef108f7c2e97bef026d386e0ecc8b3f80d4f66
unknown <4ingridsilva4@gmail.com>
2022-05-02 16:47:27 -0300

    criação exemplo nuxt.js

---
+++ exemplo-front/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Nuxt.js - bem vindo!</h1>
+    <ul>
+      <li>Elemento 1</li>
+      <li>Elemento 2</li>
+      <li>Elemento 3</li>
+      </ul>
+  </div>
+  <!-- Tutorial/-->
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+h1{
+  color: rgb(255, 2, 150)
+}
+</style>
